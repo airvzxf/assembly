4e0c9edc589fd7abaaa5e402c6c42aa6d278a328
Israel Roldan <israel.alberto.rv@gmail.com>
2017-11-21 20:12:36 -0500

    Removed the binaries for funcitions because there are not necessary only the .asm file.

--- x86_64/tutorial/asmtutor/makefiles.sh
+++ x86_64/tutorial/asmtutor/makefiles.sh
@@ -12,17 +12,11 @@ ld -m elf_i386 03-helloworld-len.o -o out_03-helloworld-len
 nasm -f elf32 -g -Fdwarf 04-subroutines.asm
 ld -m elf_i386 04-subroutines.o -o out_04-subroutines
 
-nasm -f elf32 -g -Fdwarf 05-functions.asm
-ld -m elf_i386 05-functions.o -o out_05-functions
-
 nasm -f elf32 -g -Fdwarf 05-helloworld-inc.asm
 ld -m elf_i386 05-helloworld-inc.o -o out_05-helloworld-inc
 
 nasm -f elf32 -g -Fdwarf 06-helloworld-inc.asm
 ld -m elf_i386 06-helloworld-inc.o -o out_06-helloworld-inc
-
-nasm -f elf32 -g -Fdwarf 07-functions.asm
-ld -m elf_i386 07-functions.o -o out_07-functions
 
 nasm -f elf32 -g -Fdwarf 07-helloworld-lf.asm
 ld -m elf_i386 07-helloworld-lf.o -o out_07-helloworld-lf
