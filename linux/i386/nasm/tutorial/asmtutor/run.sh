5e26e83c359a8b790d8e4130140a6f42627663d8
Israel Roldan <israel.alberto.rv@gmail.com>
2020-11-02 14:32:42 -0600

    Removed the option -e from all the echos.

--- linux/i386/nasm/tutorial/asmtutor/run.sh
+++ linux/i386/nasm/tutorial/asmtutor/run.sh
@@ -2,57 +2,57 @@
 
 ./makefiles.sh
 
-echo -e ""
-echo -e "Lesson 01 | Hello world!"
-echo -e "------------------------------------------------------------"
+echo ""
+echo "Lesson 01 | Hello world!"
+echo "------------------------------------------------------------"
 ./out_01-hello
 
-echo -e ""
-echo -e "Lesson 02 | Hello world!"
-echo -e "------------------------------------------------------------"
+echo ""
+echo "Lesson 02 | Hello world!"
+echo "------------------------------------------------------------"
 ./out_02-helloworld
 
-echo -e ""
-echo -e "Lesson 03 | Calculate string length"
-echo -e "------------------------------------------------------------"
+echo ""
+echo "Lesson 03 | Calculate string length"
+echo "------------------------------------------------------------"
 ./out_03-helloworld-len
 
-echo -e ""
-echo -e "Lesson 04 | Subroutines"
-echo -e "------------------------------------------------------------"
+echo ""
+echo "Lesson 04 | Subroutines"
+echo "------------------------------------------------------------"
 ./out_04-subroutines
 
-echo -e ""
-echo -e "Lesson 05 | External include files"
-echo -e "------------------------------------------------------------"
+echo ""
+echo "Lesson 05 | External include files"
+echo "------------------------------------------------------------"
 ./out_05-helloworld-inc
 
-echo -e ""
-echo -e "Lesson 06 | NULL terminating bytes"
-echo -e "------------------------------------------------------------"
+echo ""
+echo "Lesson 06 | NULL terminating bytes"
+echo "------------------------------------------------------------"
 ./out_06-helloworld-inc
 
-echo -e ""
-echo -e "Lesson 07 | Linefeeds"
-echo -e "------------------------------------------------------------"
+echo ""
+echo "Lesson 07 | Linefeeds"
+echo "------------------------------------------------------------"
 ./out_07-helloworld-lf
 
-echo -e ""
-echo -e "Lesson 08 | Passing arguments"
-echo -e "------------------------------------------------------------"
+echo ""
+echo "Lesson 08 | Passing arguments"
+echo "------------------------------------------------------------"
 ./out_08-helloworld-args "This is one argument" "This is another" 101
 
-echo -e ""
-echo -e "Lesson 09 | User input"
-echo -e "------------------------------------------------------------"
-echo "Mr. Robot" | ./out_09-helloworld-input
+echo ""
+echo "Lesson 09 | User input"
+echo "------------------------------------------------------------"
+./out_09-helloworld-input <<< "Mr. Robot"
 
-echo -e ""
-echo -e "Lesson 10 | Count to 10"
-echo -e "------------------------------------------------------------"
+echo ""
+echo "Lesson 10 | Count to 10"
+echo "------------------------------------------------------------"
 ./out_10-helloworld-10
 
-echo -e ""
-echo -e "Lesson 11 | Count to 10 (itoa)"
-echo -e "------------------------------------------------------------"
+echo ""
+echo "Lesson 11 | Count to 10 (itoa)"
+echo "------------------------------------------------------------"
 ./out_11-helloworld-itoa
